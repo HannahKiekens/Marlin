9903d3b4ed002f64052f5929cde290ebdc624717
Yannick Kiekens <yannickkiekens@gmail.com>
2016-11-01 16:51:35 +0100

    Enable logging

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -658,7 +658,7 @@
 // Enable this feature to get detailed logging of G28, G29, M48, etc.
 // Logging is off by default. Enable this logging feature with 'M111 S32'.
 // NOTE: Requires a huge amount of PROGMEM.
-//#define DEBUG_LEVELING_FEATURE
+#define DEBUG_LEVELING_FEATURE
 
 #if ENABLED(AUTO_BED_LEVELING_FEATURE)
 
